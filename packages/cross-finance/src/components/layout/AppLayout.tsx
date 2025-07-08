36baa4ac6d20f456d6826d58247cb37167e913a3
Malek <stonemokkaks@gmail.com>
2025-07-08 19:22:00 +0300

    added initial new design for dashboard

--- packages/cross-finance/src/components/layout/AppLayout.tsx
+++ packages/cross-finance/src/components/layout/AppLayout.tsx
@@ -1,17 +1,16 @@
 
-import { SidebarProvider } from "@/components/ui/sidebar";
-import { AppSidebar } from "@/components/layout/AppSidebar";
 import { Outlet } from "react-router-dom";
+import Header from "./Header";
 
 export function AppLayout() {
   return (
-    <SidebarProvider>
-      <div className="min-h-screen flex w-full bg-background dark">
-        <AppSidebar />
-        <main className="flex-1 overflow-y-auto">
+    // <SidebarProvider>
+        <main className=" ">
+          <header className="bg-[#141414]">
+          <Header />
+          </header>
           <Outlet />
         </main>
-      </div>
-    </SidebarProvider>
+    // </SidebarProvider> 
   );
 }
